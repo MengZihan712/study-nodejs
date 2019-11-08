ae1122f1061525d04bf175340c981b74de3a2d77
wanghui <2052832414@qq.com>
2019-11-08 16:59:52 +0800

    1

--- httpdemo/demo1.js
+++ httpdemo/demo1.js
@@ -4,17 +4,35 @@ const fs = require('fs')
 
 let server = http.createServer();
 
-server.on('request',function(req,res){
-let url = req.url;
-let demo = "./data"
+server.on('request', function(req, res) {
+	let url = req.url;
+	let demo = "/data"
+	if (url === "/") {
+		fs.readFile('D:/Test/Study/StudyNode/study-nodejs/httpdemo/data/index.html', function(error, data) {
+			if (error) {
+				return res.end(error)
+			} else {
+				res.end(data)
+			}
+		})
+	}else if(url === "/kt1") {
+		fs.readFile('D:/Test/Study/StudyNode/study-nodejs/httpdemo/data/kt1.jpg', function(error, data) {
+			if (error) {
+				return res.end(error)
+			} else {
+				res.end(data)
+			}
+		})
+	}else if(url === "/main.css") {
+		fs.readFile('D:/Test/Study/StudyNode/study-nodejs/httpdemo/data/main.css', function(error, data) {
+			if (error) {
+				return res.end(error)
+			} else {
+				res.end(data)
+			}
+		})
+	}
 
-    fs.readFile(demo+url,function(error,data){
-        if(error){
-            console.log(error)
-        }else{
-            res.end(data)
-        }
-    })
 
 })
 
@@ -22,7 +40,7 @@ let demo = "./data"
 
 
 
-server.listen(3000,function () {
-    console.log("启动成功")
-    
+server.listen(3000, function() {
+	console.log("启动成功")
+
 })
